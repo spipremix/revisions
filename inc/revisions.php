42a050e20630a479789adfc8077f005b60c4bdff
Cerdic <cedric@yterium.com>
2010-06-17 11:32:13 +0000

    eviter d'attendre 10s a chaque enregistrement de revision pour cause d'imprecision de SQL (equipement) report de http://trac.rezo.net/trac/spip/changeset/15778

--- inc/revisions.php
+++ inc/revisions.php
@@ -418,7 +418,7 @@ function ajouter_version($id_objet,$objet, $champs, $titre_version = "", $id_aut
 	// distinctif (pour eviter la violation d'unicite)
 	// et un titre contenant en fait le moment de l'insertion
 	list($ms, $sec) = explode(' ', microtime());
-	$date = $sec . substr($ms,1);
+	$date = $sec . substr($ms,1,4); // SQL ne ramene que 4 chiffres significatifs apres la virgule pour 0.0+titre_version
 	$datediff = ($sec - mktime(0,0,0,9,1,2007)) * 1000000 + substr($ms,2, strlen($ms)-4);
 
 	$valeurs = array('id_objet' => $id_objet,
