d66f55e5bf7c2169b72fbeccbfb3265fbef50b21
Cerdic <cedric@yterium.com>
2011-03-20 14:31:46 +0000

    Page revision en squelette, pour afficher le diff d'une revision, avec le formulaire qui permet de sélectionner quelles versions on veut comparer

--- lang/revisions_fr.php
+++ lang/revisions_fr.php
@@ -4,41 +4,29 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	// A
-	'articles' => 'Les articles',
-
 	// B
-	'breves' => 'Les br&egrave;ves',
+	'bouton_comparer' => 'Comparer',
 
-	// G
-	'groupes_mots' => 'Les groupes de mots cl&eacute;s',
-
+	// D
+	'differences_versions' => 'Diff&eacute;rences entre version N<sup>o</sup>@id_version@ et version N<sup>o</sup>@id_diff@',
+	
 	// I
 	'info_aucune_revision'=> 'Aucune r&eacute;vision',
 	'info_1_revision'=> '1 r&eacute;vision',
 	'info_nb_revisions'=> '@nb@ r&eacute;visions',
 	
 	'icone_restaurer_version' => 'Restaurer cette version',
-	'icone_retour_breve' => 'Retour &agrave; la br&egrave;ve',
-	'icone_retour_groupe_mot' => 'Retour au groupe de mots cl&eacute;s',
-	'icone_retour_mot' => 'Retour au mot cl&eacute;',
-	'icone_retour_rubrique' => 'Retour &agrave; la rubrique',
 	'icone_suivi_revisions' => 'Suivi des r&eacute;visions',
 
 	// L
 	'label_config_revisions_objets' => 'Sur quels objets activer les r&eacute;visions&nbsp;:',
-
-	// M
-	'mots' => 'Les mots cl&eacute;s',
+	'label_choisir_id_version'=>'S&eacute;lectionnez les versions &agrave; comparer',
 
 	// O
 	'objet_editorial' => 'objet',
 
 	// P
 	'plugin_update' => 'Mise &agrave; jour du plugin "R&eacute;visions" en version @version@.',
-
-	// R
-	'rubriques' => 'Les rubriques',
 
 	// T
 	'titre_form_revisions_objets' => 'Activer les r&eacute;visions',
