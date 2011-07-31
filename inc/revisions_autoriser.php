c35f9dbca0d81d5db48e8d48903eb29c057c0145
Cerdic <cedric@yterium.com>
2011-07-31 18:17:03 +0000

    http://core.spip.org/issues/2176 : autoriser('menu',xxx) au lieu de  autoriser('bouton',xxx)

--- inc/revisions_autoriser.php
+++ inc/revisions_autoriser.php
@@ -35,7 +35,7 @@ function autoriser_voirrevisions_dist($faire, $type, $id, $qui, $opt) {
 }
 
 // tout le monde peut voir le bouton de suivi des revisions
-function autoriser_suivi_revisions_bouton_dist($faire, $type='', $id=0, $qui = NULL, $opt = NULL){
+function autoriser_suivi_revisions_menu_dist($faire, $type='', $id=0, $qui = NULL, $opt = NULL){
 	return true;
 }
 
