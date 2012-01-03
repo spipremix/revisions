f11cf47b12eb6928cd39274dc35d172119caa0e7
jrynik <jrynik@yahoo.co.uk>
2012-01-03 19:44:53 +0000

    langues (paquet-revisions)

--- lang/paquet-revisions_sk.php
+++ lang/paquet-revisions_sk.php
@@ -8,8 +8,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// R
-	'revisions_description' => 'Suivi des modifications des objets éditoriaux', # NEW
-	'revisions_slogan' => 'Suivi des modifications des objets éditoriaux' # NEW
+	'revisions_description' => 'Sledujte zmeny redakčných objektov',
+	'revisions_slogan' => 'Sledovanie zmien redakčných objektov'
 );
 
 ?>
