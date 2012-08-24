bd96902f1b9a53d9f8155cd7d54643b40d786e36
salvatore <salvatore@rezo.net>
2012-08-24 21:50:08 +0000

    langues (paquet-revisions)

--- lang/paquet-revisions_nl.php
+++ lang/paquet-revisions_nl.php
@@ -8,10 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// R
-	'revisions_description' => 'De revisies van de editoriale objecten volgen', # Robert & Van Dale: revisie de (v.)
-1 herziening - révision (v.) -  context: revisie van de drukproef, révision, correction de l'épreuve
-2 periodieke controle - révision (v.) vérification (v.) 
-Vu la similarité avec le fr je opense que c'est le meilleur terme (Wijzigingen = changements, c'est pas pareil)
+	'revisions_description' => 'De revisies van de editoriale objecten volgen', # Robert & Van Dale: revisie de (v.) 1 herziening - révision (v.) -  context: revisie van de drukproef, révision, correction de l'épreuve 2 periodieke controle - révision (v.) vérification (v.)  Vu la similarité avec le fr je opense que c'est le meilleur terme (Wijzigingen = changements, c'est pas pareil)
 	'revisions_slogan' => 'Revisies laat je toe de  veranderingen van de editorialen objecten zien en volgen.'
 );
 
