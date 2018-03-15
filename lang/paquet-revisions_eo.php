abab3496a19e787fcd299c898dbe50920067cce4
gpl <gpl@long-player.net>
2018-03-15 04:42:10 +0000

    [Salvatore] [source:_core_/plugins/revisions/lang/ paquet-revisions] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/paquet-revisions_eo.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-revisions?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'revisions_description' => 'Superrigardo de la modifoj de la redaktaj objektoj',
+	'revisions_slogan' => 'Superrigardo de la modifoj de la redaktaj objektoj'
+);
