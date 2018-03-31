29bde21fdf8fab9238f333d547ab77fd76878586
spip.franck <spip.franck@lien-d-amis.net>
2018-03-31 22:42:51 +0000

    Bonne année aussi aux plugins-dist :-D

--- revisions_autoriser.php
+++ revisions_autoriser.php
@@ -1,9 +1,9 @@
-<?php
+﻿<?php
 
 /***************************************************************************\
  *  SPIP, Systeme de publication pour l'internet                           *
  *                                                                         *
- *  Copyright (c) 2001-2017                                                *
+ *  Copyright (c) 2001-2018                                                *
  *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
  *                                                                         *
  *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
