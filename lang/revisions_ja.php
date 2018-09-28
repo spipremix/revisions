c97e17f33f7bd2653e040e4d34d5bfc459d469f0
origenius <origenius@mvb.biglobe.ne.jp>
2018-09-28 03:40:16 +0000

    [Salvatore] [source:_core_/plugins/revisions/lang/ revisions] Export depuis http://trad.spip.net de la langue ja

--- lang/revisions_ja.php
+++ lang/revisions_ja.php
@@ -9,19 +9,51 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// B
+	'bouton_comparer' => '改訂を表示',
+
 	// D
-	'diff_para_ajoute' => '短い記事を追加した',
-	'diff_para_deplace' => '短い記事を変更した',
-	'diff_para_supprime' => '短い記事を削除した',
-	'diff_texte_ajoute' => 'テキストを追加した',
-	'diff_texte_deplace' => 'テキストを変更した',
-	'diff_texte_supprime' => '文章を削除した',
+	'diff_para_ajoute' => '追加された段落',
+	'diff_para_deplace' => '移動された段落',
+	'diff_para_supprime' => '削除された段落',
+	'diff_texte_ajoute' => '追加されたテキスト',
+	'diff_texte_deplace' => '移動されたテキスト',
+	'diff_texte_supprime' => '削除されたテキスト',
+	'differences_versions' => 'バージョン番号@id_version @とバージョン番号@id_diff@の違い',
 
 	// I
-	'info_historique' => 'リビジョン（修正）:',
-	'info_historique_lien' => '変更のリストを表示',
-	'info_historique_titre' => '改訂（リビジョン）の追跡',
+	'icone_restaurer_version' => 'バージョン番号@version@を復元します。',
+	'icone_suivi_revisions' => '改訂の管理',
+	'info_1_revision' => '改訂1件',
+	'info_aucune_revision' => '改訂無し',
+	'info_historique' => '改訂：',
+	'info_historique_lien' => 'バージョンのリストを表示する',
+	'info_historique_titre' => '改訂の管理',
+	'info_nb_revisions' => '改訂@nb@件',
+
+	// L
+	'label_choisir_id_version' => '比較するはずのバージョンを選択してください。',
+	'label_config_revisions_objets' => '改訂を有効にする項目：',
+
+	// M
+	'message_valider_recuperer_version' => 'フォームを有効して、このバージョンに戻ります。',
+
+	// O
+	'objet_editorial' => '項目',
+
+	// P
+	'plugin_update' => 'バージョン@version@のプラグイン 「改訂」の更新。',
+
+	// T
+	'titre_form_revisions_objets' => '改訂を有効にする',
+	'titre_restaurer_version' => 'バージョンを復元する',
+	'titre_revisions' => '改訂',
 
 	// V
-	'version_initiale' => '最初のバージョン'
+	'version_deplace_rubrique' => '<b>「@from@」</b>より<b>「@to@」</b> に移動されました。',
+	'version_initiale' => '最初のバージョン',
+	'voir_differences' => '改訂を見る',
+	'voir_mes_revisions' => '私の改訂',
+	'voir_revisions' => '改訂（@id_objet@@objet@：@titre@）を見る',
+	'voir_toutes_les_revisions' => '全改訂'
 );
